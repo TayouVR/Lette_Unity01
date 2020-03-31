031e7cc983478cefae26baba80e793a490079cf7
Tayou <tayou@gmx.net>
2020-03-31 17:39:50 +0200

    improved weapon script

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,27 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour {
-	public Transform target;
 	public Transform rotator;
 	public Transform bulletSpawnpoint;
 	public GameObject bulletPrefab;
+	private Vector3 _target;
 	private GameObject _bulletClone;
 	public float initialBulletForce = 10.0f;
+
+	private void Start()
+	{
+		_target = new Vector3();
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		Vector3 mouse = Input.mousePosition;
+		Debug.Log(Input.mousePosition.x - (Screen.width/2));
 		Ray castPoint = Camera.main.ScreenPointToRay(mouse);
 		if (Physics.Raycast(castPoint, out var hit, Mathf.Infinity))
 		{
-			target.position = new Vector3(hit.point.x, hit.point.y + 0.2f, hit.point.z);
+			_target = new Vector3(hit.point.x, hit.point.y + 0.2f, hit.point.z);
 		} else {
-			target.position = new Vector3(hit.point.x, rotator.position.y, hit.point.z);
+			_target = new Vector3(rotator.position.x + (Input.mousePosition.x - (Screen.width/2)), rotator.position.y,rotator.position.z + (Input.mousePosition.y - (Screen.height/2)));
 		}
-		rotator.transform.LookAt(target, Vector3.up);
+		rotator.transform.LookAt(_target, Vector3.up);
 		if (Input.GetButton("Fire1")) {
 			_bulletClone = Instantiate(bulletPrefab, bulletSpawnpoint.position, bulletSpawnpoint.rotation);
 			_bulletClone.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, initialBulletForce));
