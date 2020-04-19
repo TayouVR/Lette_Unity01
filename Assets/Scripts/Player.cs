8cfb927e0905904bb14c816751d76331892de7f4
Tayou <tayou@gmx.net>
2020-04-19 14:30:00 +0200

    improved player jump floor detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,30 +15,22 @@ public class Player : MonoBehaviour {
 #pragma warning restore CS0108 // Element blendet vererbte Element aus; fehlendes 'new'-Schlüsselwort
 
 	// Start is called before the first frame update
-	void Start()
-    {
+	void Start() {
 		_rigidbody = this.GetComponent<Rigidbody>();
 	}
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
 		_rigidbody.AddForce(Input.GetAxis("Horizontal") * 2, 0.0f, Input.GetAxis("Vertical") * 2);
 		if (_isTouchingSomething) {
 			_rigidbody.AddForce(_jumpDirection * Input.GetAxis("Jump"));
 		}
 	}
-
-	private void OnCollisionEnter(Collision collision)
-	{
-		_isTouchingSomething = true;
-	}
-	private void OnCollisionExit(Collision collision)
-	{
+	private void OnCollisionExit(Collision collision) {
 		_isTouchingSomething = false;
 	}
-	private void OnCollisionStay(Collision collision)
-	{
+	private void OnCollisionStay(Collision collision) {
+		_isTouchingSomething = true;
 		Vector3 direction = (transform.position - collision.contacts[0].point);
 		_jumpDirection = new Vector3(direction.x * jumpForce, direction.y * jumpForce + upIncrease, direction.z * jumpForce);
 		if (showDebugRay) {
