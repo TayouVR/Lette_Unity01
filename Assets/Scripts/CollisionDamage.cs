5842d325738d1f5650b8ee490a60a61c3ae678fd
Tayou <tayou@gmx.net>
2020-04-27 16:46:38 +0200

    renamed CollisionDeath into CollisionDamage and added functionality

---
+++ Assets/Scripts/CollisionDamage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour {
+    
+    public float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<Player>() != null) {
+            other.gameObject.GetComponent<Player>().GetDamage(damage);
+        }
+    }
+}
